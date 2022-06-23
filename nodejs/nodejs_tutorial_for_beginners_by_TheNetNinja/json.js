ae6bde2975ce66d97fff4766c90e01cbcf7b546a
shivshivshivshiv <durgaprasad.myway.s@gmail.com>
2022-06-23 17:17:48 +0530

    JSON data - 17

---
+++ nodejs/nodejs_tutorial_for_beginners_by_TheNetNinja/json.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var fs = require("fs");
+
+let PORT = 3000;
+
+var server = http.createServer((req, res) => {
+  console.log("request received @" + req.url);
+  res.writeHead(200, { "Content-Type": "application/json" });
+  var myObj = {
+    name: 'You',
+    job: 'dev',
+    age: 'ice age'
+  }
+  res.end(JSON.stringify(myObj));
+});
+
+server.listen(PORT, "127.0.0.1");
+console.log("Your Server App is now ready and listening to Port " + PORT);
+console.log("Click to Test in a browser >> http://localhost:3000/mama-mia");
